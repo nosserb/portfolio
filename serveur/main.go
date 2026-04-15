9bde51f74bc956a9f8beb293f9505695639e9f40
nosserb <guylann.bresson.gb@gmail.com>
2026-04-15 18:10:54 +0200

    feat: add 404 page

--- serveur/main.go
+++ serveur/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -12,7 +15,28 @@ func main() {
 
 	// Servir les fichiers statiques
 	fs := http.FileServer(http.Dir(portfolioPath))
-	http.Handle("/", fs)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		cleanPath := filepath.Clean("/" + r.URL.Path)
+		relativePath := strings.TrimPrefix(cleanPath, "/")
+		fullPath := filepath.Join(portfolioPath, relativePath)
+
+		fileInfo, err := os.Stat(fullPath)
+		if err == nil {
+			if fileInfo.IsDir() {
+				indexPath := filepath.Join(fullPath, "index.html")
+				if _, indexErr := os.Stat(indexPath); indexErr == nil {
+					http.ServeFile(w, r, indexPath)
+					return
+				}
+			} else {
+				fs.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		w.WriteHeader(http.StatusNotFound)
+		http.ServeFile(w, r, filepath.Join(portfolioPath, "404.html"))
+	})
 
 	// Configuration et démarrage du serveur
 	port := "2525"
